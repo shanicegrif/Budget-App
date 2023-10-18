8437adf3c772911886c7c67f76b7bfcfee2b295d
Shanice <shanicegriffin@pursuit.org>
2023-10-18 19:21:07 -0400

    index comp started and partically working

--- Components/Index.jsx
+++ Components/Index.jsx
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react";
+import Transaction from "./Transaction";
+const API = import.meta.env.VITE_BASE_URL;
+
 function Index() {
+  const [transactions, setTransactions] = useState([]);
+  const [bankTotal, setBankTotal] = useState(0)
+
+  useEffect(() => {
+    fetch(`${API}/transactions`)
+    .then((res) => res.json())
+    .then((transactions) => setTransactions(transactions))
+    .catch((error) => {
+      console.error("Error fetching data.", error)
+    })
+  })
+
     return (
-      <div className="Index">
+      <div className="Transactions">
         <h2>Index</h2>
+        <section>
+          <h3>Bank Account Total: ${bankTotal}</h3>
+          <table>
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Transaction</th>
+                <th>Amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              {transactions.map((transaction, index) =>( 
+                <Transaction key={index} transaction={transaction} id = {index}/>
+              ))}
+            </tbody>
+          </table>
+        </section>
       </div>
     );
   }
---
+++ Components/Transaction.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+function Transaction({transaction, index}) {
+    return (
+        <tr>
+        <td>
+          {transaction.date }
+        </td>
+        <td>
+         <Link to={`/transactions/${index}`}>{transaction.itemName}</Link>
+        </td>
+        <td>
+        <p>{transaction.amount}</p>
+        </td>
+      </tr>
+    );
+  }
+  
+  export default Transaction;
